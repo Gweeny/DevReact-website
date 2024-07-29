67c088dc0dde8099150ec70e7aad423609c43ab6
Sam <samya.hussain@hotmail.fr>
2024-07-29 11:22:05 +0200

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,17 @@
-//Imports :
+// Imports
 import { Roboto_Mono, League_Spartan } from "next/font/google";
 import "./globals.css";
 import Header from "./Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-//variables :
+// Variables
 const robotoMono = Roboto_Mono({ weight: ["400", "700"], subsets: ["latin"] });
 const leagueSpartan = League_Spartan({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
 
+// Metadata
 export const metadata = {
   title: "DevReact Creations",
   description:
@@ -21,7 +22,7 @@ export const metadata = {
     url: "https://www.devreactcreations.com/",
     site_name: "DevReact Creations",
     title: "DevReact Creations",
-    image: "https://zupimages.net/up/24/31/kceq.png",
+    image: "https://zupimages.net/up/24/31/kceq.png", // URL de l'image à utiliser pour les aperçus
     description:
       "Développez votre présence en ligne avec DevReact Creations, spécialiste en creation de sites web personnalisés utilisant ReactJS et NodeJS.",
   },
@@ -31,40 +32,47 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="DevReact Creations" />
-        <meta
-          name="description"
-          content="Développez votre présence en ligne avec DevReact Creations, spécialiste en creation de sites web personnalisés utilisant ReactJS et NodeJS."
-        />
+
+        {/* SEO Meta Tags */}
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
         <meta
           name="keywords"
           content="ReactJS, NodeJS, Développement Web, Portfolio, DevReact Creations, DevReact Creation, DevReact, site web react, react website"
         />
 
-        <meta property="og:url" content="https://www.devreactcreations.com" />
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content={metadata.openGraph.title} />
         <meta
-          property="og:image"
-          content="https://zupimages.net/viewer.php?id=24/31/kceq.png"
+          property="og:description"
+          content={metadata.openGraph.description}
         />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.site_name} />
+        <meta property="og:image" content={metadata.openGraph.image} />
 
+        {/* Structured Data with JSON-LD */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: `
-          {
-            "@context": "https://schema.org",
-            "@type": "WebSite",
-            "name": "DevReact Creations",
-            "url": "https://www.devreactcreations.com",
-            "description": "Obtenez un site web qui vous ressemble !",
-            "author": {
-              "@type": "Person",
-              "name": "Samya Hussain"
-            }
-          }
-          `,
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "DevReact Creations",
+              url: "https://www.devreactcreations.com",
+              description:
+                "Développez votre présence en ligne avec DevReact Creations, spécialiste en creation de sites web personnalisés utilisant ReactJS et NodeJS.",
+              author: {
+                "@type": "Person",
+                name: "Samya Hussain",
+              },
+            }),
           }}
         />
       </head>
